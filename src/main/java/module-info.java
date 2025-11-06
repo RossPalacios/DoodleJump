ef7b3957b7317ea26b6cff4f9b05b523516fb06d
RossPalacios <rosspalacios998@gmail.com>
2025-11-06 11:46:17 -0600

    First test commit

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.doodlejump.doodlejump {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.doodlejump.doodlejump to javafx.fxml;
-    exports com.doodlejump.doodlejump;
-}
