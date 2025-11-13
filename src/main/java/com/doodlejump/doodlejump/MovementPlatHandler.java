105fbd559b069f8d5cda0a4853bc52f3930b4543
RossPalacios <rosspalacios998@gmail.com>
2025-11-13 11:56:48 -0600

    collision for x bounds works just not yet for y

--- src/main/java/com/doodlejump/doodlejump/MovementPlatHandler.java
+++ src/main/java/com/doodlejump/doodlejump/MovementPlatHandler.java
@@ -2,7 +2,11 @@ package com.doodlejump.doodlejump;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.geometry.Bounds;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
 import javafx.util.Duration;
 
 import java.util.ArrayList;
@@ -83,14 +87,78 @@ public class MovementPlatHandler {
     }
 
     private void checkPlatformCollision() {
+
         for (Platform platform : platforms) {
-            if (player.getBoundsInParent().intersects(platform.getBoundsInParent())) {
+            if (isColliding(platform, player)) {
                 if (velocity > 0 && player.getY() + player.getFitHeight() - velocity <= platform.getY()) {
                     player.setY(platform.getY() - player.getFitHeight());
                     velocity = REBOUND_VELOCITY;
                 }
             }
         }
+    }
+
+    private boolean isColliding(ImageView imgOne, ImageView imgTwo) {
+
+
+        // create pixel reader to check collision
+        PixelReader pixelReaderOne = imgOne.getImage().getPixelReader();
+        PixelReader pixelReaderTwo = imgTwo.getImage().getPixelReader();
+
+        // if there is no pixels then there's nothing to check.
+        if (pixelReaderOne == null || pixelReaderTwo == null) {
+            return false;
+        }
+
+        // calculating regions which overlap
+        Bounds boundOne = imgOne.getBoundsInParent();
+        Bounds boundTwo = imgTwo.getBoundsInParent();
+
+        // much faster check.
+        if (!boundOne.intersects(boundTwo)) return false;
+
+
+        int startX = (int) Math.max(boundOne.getMinX(), boundTwo.getMinX()); // leftmost x
+        int startY = (int) Math.max(boundOne.getMinY(), boundTwo.getMinY()); // lowest y
+        int endX = (int) Math.min(boundOne.getMaxX(), boundTwo.getMaxX()); // higher x
+        int endY = (int) Math.min(boundOne.getMaxY(), boundTwo.getMaxY()); // higher y
+        //--------------
+
+        // really annoying but scaling is needed due to using getFitWidth/height
+        double scaleXOne = imgOne.getImage().getWidth() / imgOne.getFitWidth();
+        double scaleYOne = imgOne.getImage().getHeight() / imgOne.getFitHeight();
+        double scaleXTwo = imgTwo.getImage().getWidth() / imgTwo.getFitWidth();
+        double scaleYTwo = imgTwo.getImage().getHeight() / imgTwo.getFitHeight();
+
+        //Looping through all pixels, very obnoxious
+        for (int y = startY; y < endY; y++) {
+            for (int x = startX; x < endX; x++) {
+
+                int x1 = (int) ((x - boundOne.getMinX()) * scaleXOne);
+                int y1 = (int) ((y - boundOne.getMinY()) * scaleYOne);
+                int x2 = (int) ((x - boundTwo.getMinX()) * scaleXTwo);
+                int y2 = (int) ((x - boundTwo.getMinX()) * scaleYTwo);
+
+                // make sure values are correctly in bounds.
+                if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0)
+                    continue;
+
+                //get pixel at the correct coordinates
+                int pixIntOne = pixelReaderOne.getArgb(x1, y1);
+                int pixIntTwo = pixelReaderTwo.getArgb(x2, y2);
+
+                //this gets alpha channels, whatever those are, I did have to look this up
+                // since I have never made pixel perfect collision before in javaFX.
+                int alphaOne = pixIntOne >> 24 & 0xFF;
+                int alphaTwo = pixIntTwo >> 24 & 0xFF;
+
+                //if they aren't transparent then they are colliding
+                if (alphaOne > 0 && alphaTwo > 0) {
+                    return true;
+                }
+            }
+        }//end of for loops
+        return false;
     }
 
     // simple infinite scrolling
